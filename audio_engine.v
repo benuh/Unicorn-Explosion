9ac2f190c4951eef1c2213e033a44050f5590d3e
akswanson <akswanson@cpp.edu>
2018-11-19 20:09:15 -0800

    added audio_engine file, no comments yet  Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   audio_engine.v
    	new file:   audio_engine_tb.v

--- audio_engine.v
+++ audio_engine.v
@@ -1,2 +1,95 @@
-module audio_engine(input CLK100MHZ, input jump, input isdead, output audio);
+`timescale 1ns / 1ps
 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Team 1
+// Engineer: Andrew Swanson
+// 
+// Create Date: 11/17/18
+// Design Name: Audio Engine
+// Module Name: audio_engine
+// Project Name: Unicorn_explosion
+// Target Devices: Nexys DDR4
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module audio_engine(input CLK100MHZ, input jump, input isdead, output reg audio_out, output reg power);
+
+reg audio;
+reg [23:0]counter;
+reg low_pitch;
+reg high_pitch;
+reg [3:0]mod_counter;
+reg [23:0] high_counter;
+reg [23:0] low_counter;
+
+always@(CLK100MHZ) //Modifies mod_counter and audio_out
+    begin
+    if(mod_counter == 0)
+        begin
+        audio_out = 0;
+        end
+    else if(mod_counter == 15)
+        begin
+        audio_out = 1;
+        end
+    else
+        begin
+        audio_out = audio;
+        end
+    mod_counter = mod_counter + 1;
+    end
+
+
+always@(posedge CLK100MHZ) //modifies power, low_counter, low_pitch, high pitch, high counter, 
+begin
+power = 1;
+if(isdead)
+	begin
+	if(counter > low_counter)
+		begin
+		counter <= 0;
+		low_pitch <= ~low_pitch;
+		low_counter <= low_counter + 8'd128;
+		end
+	else
+		counter = counter + 1;
+	end
+else if(jump)
+	begin
+	if(counter > high_counter)
+		begin
+		counter <= 0;
+		high_pitch <= ~high_pitch;
+		high_counter <= high_counter - 8'd128;
+		end
+    else
+        counter = counter + 1;
+	end
+else
+	begin
+	high_counter = 100000;
+	low_counter = 200000;
+	counter = counter + 1;
+	end
+
+end
+
+always@(CLK100MHZ)
+begin
+	if(isdead)
+		audio = low_pitch;
+	else if(jump)
+		audio = high_pitch;
+	else
+		audio = 1'b0;
+end
+endmodule
---
+++ audio_engine_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2018 12:32:54 AM
+// Design Name: 
+// Module Name: audio_engine_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module audio_engine_tb(
+
+    );
+endmodule
